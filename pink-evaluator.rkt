81b64d40144f86357da4caf3c3a1c2faa1c18f49
Brian Lachance <blachance@gmail.com>
2018-04-17 20:37:24 -0400

    Parameterize the minimal Pink evaluator by environment operations
    This makes it easy to play with defunctionalized/functionalized
    environments. Everything in the paper was functionalized, so I wanted
    to see if any surprises come from defunctionalizing. None did!

--- pink-evaluator.rkt
+++ pink-evaluator.rkt
@@ -1,7 +1,7 @@
 #lang s-exp (submod "racket-updown.rkt" updown)
-(define (eval-poly maybe-lift eval exp env)
+(define ((mk/eval-poly lookup extend) maybe-lift eval exp env)
   (if (number? exp) (maybe-lift exp)
-  (if (symbol? exp) (env exp)
+  (if (symbol? exp) (lookup exp env)
   (if (symbol? (car exp))
       (let ([rator (car exp)])
         (if (eq? 'lambda rator)
@@ -9,12 +9,8 @@
                    [formal (car (caddr exp))]
                    [body (cadddr exp)]
                    [f (lambda f (x)
-                              (let ([env* (lambda (y) (if (eq? y self)
-                                                          f
-                                                          (if (eq? y formal)
-                                                              x
-                                                              (env y))))])
-                                (eval body env*)))])
+                        (let ([env* (extend self f (extend formal x env))])
+                          (eval body env*)))])
               (maybe-lift f))
         (if (eq? '+ rator) (+ (eval (cadr exp) env) (eval (caddr exp) env))
         (if (eq? '- rator) (- (eval (cadr exp) env) (eval (caddr exp) env))
@@ -23,23 +19,62 @@
         (if (eq? 'if rator) (if (eval (cadr exp) env)
                                 (eval (caddr exp) env)
                                 (eval (cadddr exp) env))
-        ((env rator) (eval (cadr exp) env)))))))))
+        ((lookup rator env) (eval (cadr exp) env)))))))))
       (let ([rator (eval (car exp) env)]
             [rand (eval (cadr exp) env)])
         (rator rand))))))
 
-(define (eval0 exp env) (eval-poly (lambda (v) v) eval0 exp env))
-(define (eval exp) (eval0 exp (lambda (x) ('unbound x))))
-(module+ test
-  (check-equal? (eval '(+ 40 2)) 42)
-  (check-equal? (eval '(- 5 7)) -2)
-  (check-equal? (eval '(if (eq? 0 0) 1 0)) 1)
-  (check-equal? (eval '(if (eq? 0 0) 0 1)) 0)
+(define emptyenv/d '())
+(define (lookup/d x env)
+  (if (null? env)
+      ('unbound x)
+      (let ([y (car env)]
+            [v (cadr env)]
+            [env-rest (cdr (cdr env))])
+        (if (eq? x y)
+            v
+            (lookup/d x env-rest)))))
+(define (extend/d x v env)
+  (cons x (cons v env)))
 
-  (define fac-src '(lambda f (n) (if (eq? n 0) 1 (* n (f (- n 1))))))
-  (check-equal? ((eval fac-src) 4) 24))
-
-(define (evalc0 exp env) (eval-poly (lambda (v) (lift v)) evalc0 exp env))
-(define (evalc exp) (evalc0 exp (lambda (x) ('unbound x))))
+(define emptyenv/f (lambda (x) ('unbound x)))
+(define (lookup/f x env) (env x))
+(define (extend/f x v env) (lambda (y) (if (eq? x y) v (lookup/f y env))))
 (module+ test
-  (check-equal? ((run 0 (evalc fac-src)) 4) 24))
+  (define env0 (extend/f 'x 0 emptyenv/f))
+  (check-equal? (lookup/f 'x env0) 0)
+
+  (define env1 (extend/f 'y 1 env0))
+  (check-equal? (lookup/f 'y env1) 1)
+  (check-equal? (lookup/f 'x env1) 0))
+
+(define (mk/eval lookup extend emptyenv)
+  (let* ([eval-poly (mk/eval-poly lookup extend)]
+         [eval (lambda eval (exp env) (eval-poly (lambda (v) v) eval exp env))])
+    (lambda (exp) (eval exp emptyenv))))
+(define (mk/evalc lookup extend emptyenv)
+  (let* ([eval-poly (mk/eval-poly lookup extend)]
+         [evalc (lambda evalc (exp env) (eval-poly (lambda (v) (lift v)) evalc exp env))])
+    (lambda (exp) (evalc exp emptyenv))))
+
+(define eval/d (mk/eval lookup/d extend/d emptyenv/d))
+(define eval/f (mk/eval lookup/f extend/f emptyenv/f))
+(module+ test
+  (check-equal? (eval/f '(+ 40 2)) 42)
+  (check-equal? (eval/f '(- 5 7)) -2)
+
+  (check-equal? (eval/f '(if (eq? 0 0) 1 0)) 1)
+  (check-equal? (eval/f '(if (eq? 0 0) 0 1)) 0)
+
+  (define id-src '(lambda _ (x) x))
+  (check-equal? ((eval/f id-src) 10) 10)
+
+  (define fac-src '(lambda fac (n) (if (eq? n 0) 1 (* n (fac (- n 1))))))
+  (check-equal? ((eval/f fac-src) 4) 24)
+  (check-equal? ((eval/d fac-src) 4) 24))
+
+(define evalc/d (mk/evalc lookup/d extend/d emptyenv/d))
+(define evalc/f (mk/evalc lookup/f extend/f emptyenv/f))
+(module+ test
+  (check-equal? ((run 0 (evalc/d fac-src)) 4) 24)
+  (check-equal? ((run 0 (evalc/f fac-src)) 4) 24))
